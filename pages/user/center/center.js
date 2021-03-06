fe95f6e7f26157137c9899d44563d0f525e1cd2b
fight1991 <359193813@qq.com>
2021-03-06 09:32:41 +0800

    显示或隐藏身份id信息

--- pages/user/center/center.js
+++ pages/user/center/center.js
@@ -5,16 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: '张三丰',
+    isShowNum: false,
+    idcard: 12312121212121212,
+    phone: 13348404848,
+    trueIdcard: '',
+    truePhone: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.showText()
   },
-
+  showText () {
+    var { isShowNum, idcard, phone } = this.data
+    var temp = !isShowNum
+    this.setData({
+      isShowNum: temp,
+      trueIdcard: temp ? this.hideText(idcard) : idcard,
+      truePhone: temp ? this.hideText(phone, 'phone') : phone
+    })
+  },
+  hideText (str, type) {
+    var startIndex = type == 'phone' ? 3 : 6
+    var endIndex = type == 'phone' ? 2 : 4
+    var tempStr = str.toString()
+    var startText = tempStr.slice(0, startIndex)
+    var endText = tempStr.toString().slice(tempStr.length - endIndex)
+    return startText + "********" + endText
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
