5c47ee80d3b395f11e1f4ea8bd4bdf28f23dca9a
fight1991 <359193813@qq.com>
2021-03-19 09:59:44 +0800

    轮询优化

--- pages/user/record_change/record_change.js
+++ pages/user/record_change/record_change.js
@@ -12,9 +12,10 @@ Page({
   data: {
     currentStep: 0,
     qrcodeText: '',
+    timer: null,
+    timeGap: 1000,
+    initToken: '', // 生成二维码的初始值
     checkStatus: 41,
-    timer: null, // 轮询计时器
-    delayTimer: null, // 延时定时器, 控制二维码失效时间暂时为 2分钟
     isRefresh: false, // 是否刷新二维码
     stepList: ['变更申请', '等待审核'],
     statusText: {
@@ -39,57 +40,63 @@ Page({
     }
     let { result } = await car_owner_change_status({qrcodeValidityToken: ''})
     if (result) {
-      // 0 无变更记录生成二维码 -1：二维码已生成、-2:二维码已失效 41:变更备案人审核中、42:变更备案人审核拒绝、43:变更备案人成功、2:取消
-      this.setData({
-        checkStatus: result.status,
-        failReason: result.failReason
-      })
+      // 处理状态分支
       this.handleStatus(result)
     }
   },
-  // 轮询开始 5s中轮询一次
-  startSearch (qrcodeToken) {
-    var timer = setInterval( async () => {
+  // 轮询开始
+  async startSearch (qrcodeToken) {
       let { result, other, error } = await car_owner_change_status({
         qrcodeValidityToken: qrcodeToken
       }, false)
-      if (result) {
-        this.setData({
-          checkStatus: result.status
-        })
-        this.handleStatus(result)
-      }
-      // 清除定时器
+      this.handleStatus(result)
       if (other || error) {
+        this.setData({
+          isRefresh: true
+        })
         this.clearTimer()
       }
-    }, 5000)
-    // 存储timeId
-    this.data.timer = timer
   },
   // 清楚定时器
   clearTimer () {
     if (this.data.timer) {
-      clearInterval(this.data.timer)
+      clearTimeout(this.data.timer)
       this.data.timer = null
     }
   },
-  // 判断状态并处理逻辑分支
+  /**
+   * 判断状态并处理逻辑分支:
+   *  0 无变更记录生成二维码
+   * -1：二维码已生成
+   * -2:二维码已失效
+   * 41:变更备案人审核中
+   * 42:变更备案人审核拒绝、
+   * 43:变更备案人成功、2:取消
+   * */ 
   handleStatus (result) {
-    // 状态为-2 二维码失效
-    if (result.status == -2) {
-      this.clearTimer()
-      this.setData({
-        isRefresh: true
-      })
-    }
-    // 状态为0, 二维码有效
+    this.setData({
+      checkStatus: result.status,
+      failReason: result.failReason
+    })
+    // 状态为0, 显示并存储二维码初始值
     if (result.status == 0) {
       this.setData({
         qrcodeText: result.qrcodeValidityToken + '&change'
       })
-      // 开始轮巡, 查看扫码的结果
-      this.startSearch(result.qrcodeValidityToken)
+      this.data.initToken = result.qrcodeValidityToken
+    }
+    // 状态为 0 或-1 二维码尚未被扫码或还在有效, 继续1s后开启轮询
+    if (result.status == -1 || result.status ==0) {
+      this.data.timer = setTimeout(() => {
+        this.startSearch(result.qrcodeValidityToken)
+      }, this.data.timeGap)
+    }
+    // 状态为-2 二维码失效
+    if (result.status == -2) {
+      this.setData({
+        isRefresh: true
+      })
+      this.clearTimer()
     }
     // 41 -等待审核页面
     if (result.status == 41) {
@@ -100,7 +107,6 @@ Page({
     }
     // 审核失败, 审核成功
     if (result.status == 42 || result.status == 43) {
-      // 清除定时器
       this.clearTimer()
       // 跳转到另外几个状态的页面
       this.routeOtherPage(result.status)
