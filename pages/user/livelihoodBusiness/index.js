2d7c93ce592f2fee9e5aa10aedf77213813eb99d
fight1991 <359193813@qq.com>
2021-04-29 13:51:10 +0800

    暂存

--- pages/user/livelihoodBusiness/index.js
+++ pages/user/livelihoodBusiness/index.js
@@ -6,6 +6,9 @@ Page({
    */
   data: {
     currentStatus: 'none', // auditing:审核中 failure:审核失败、waitInstall:待安装 registered:已登记、reportedLost:已报失、scrapped:已报废
+    bindingDialogVisible: false, // 绑定企业车弹框
+    brandNumVisible: false, // 显示车牌的弹框
+    brandNum: '',
     opList: [
       {
         label: '备案申报',
@@ -22,8 +25,8 @@ Page({
       }, {
         label: '绑定企业车',
         icon: '/pages/image/peopleChange.png',
-        clickEvent: 'routeTo',
-        pageFlag: 'record_change',
+        clickEvent: 'bindingMenu',
+        pageFlag: '',
         permission: ['auditing', 'failure', 'waitInstall', 'registered', 'reportedLost', 'none']
       }, {
         label: '一键报失',
@@ -52,6 +55,40 @@ Page({
     this.setData({
       currentStatus: e.detail.vehicleStatus
     })
+  },
+  // 退出企业
+  outCorpBtn () {
+
+  },
+  // 绑定企业车按钮
+  bindingMenu () {
+    this.setData({
+      bindingDialogVisible: true
+    })
+  },
+  // 扫描二维码
+  scanBrandCode () {
+    wx.scanCode({
+      onlyFromCamera: true,
+      scanType: ['qrCode'],
+      success: _res => {
+        this.setData({
+          bindingDialogVisible: false,
+          brandNum: _res.result || '',
+          brandNumVisible: true
+        })
+      },
+      fail: _ => {
+        wx.showToast({
+          title: '扫码失败, 请重试',
+          icon: 'error'
+        })
+      }
+    })
+  },
+  // 绑定申请
+  putBinding () {
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
