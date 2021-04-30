8bf419574b8437544abdc98ff04e594abaec8a8b
fight1991 <359193813@qq.com>
2021-04-30 10:45:08 +0800

    暂存

---
+++ pages/user/livelihoodBusiness/record/checking.js
@@ -0,0 +1,69 @@
+// pages/user/livelihoodBusiness/record/checking.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    statusText: '您的备案申请审核中，请耐心等待',
+    statusImg: '/pages/image/check-ing.png',
+    currentStep: 2, // 当前操作步骤
+    stepList: ['完善车辆信息', '绑定企业', '等待审核'],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
